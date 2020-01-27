1fb572da8a7d45a2808bb52858823c07b3099097
Yoshiki Ohashi <rrrtcdish2@gmail.com>
2020-01-27 23:34:52 +0900

    [add] FeignConfig

---
+++ src/main/java/com/biztoi/web/config/FeignConfiguration.java
@@ -0,0 +1,13 @@
+package com.biztoi.web.config;
+
+import feign.Logger;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FeignConfiguration {
+    @Bean
+    public Logger.Level feignLoggerLevel() {
+        return Logger.Level.FULL;
+    }
+}
