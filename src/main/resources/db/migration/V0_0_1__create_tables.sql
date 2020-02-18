599b11b56aa76e8934232e7c087b5cf7dc0c9252
Yoshiki Ohashi <rrrtcdish2@gmail.com>
2020-02-18 12:39:10 +0900

    [add] 一旦DDLの追加

--- src/main/resources/db/migration/V0_0_1__create_tables.sql
+++ src/main/resources/db/migration/V0_0_1__create_tables.sql
@@ -22,82 +22,44 @@ create table mst_toi
 
 create table mst_question
 (
+    id uuid not null default uuid_in(md5(random()::text || clock_timestamp()::text)::cstring),
     pattern_id int not null,
     order_id int not null,
     title varchar(255) not null,
     detail varchar(255) not null,
+    example varchar(255) not null,
     step varchar(1) not null,
+    answer_type varchar(1) not null,
     required varchar(1) default '0' not null,
     constraint mst_question_pk
-        primary key (pattern_id, order_id)
+        primary key (id)
 );
 
--- create table ANSWER_HEAD
--- (
---     ID int auto_increment comment '回答ID'
---         primary key,
---     USER_ID varchar(128) not null comment '所有者',
---     TOI_ID int not null comment '問題集と紐づくID',
---     PUBLISH_FLG bit(1) not null default 1 comment '公開フラグ',
---     INSERTED timestamp default current_timestamp,
---     MODIFIED timestamp default current_timestamp not null,
---     constraint ANSWER_USER_ID_fk
---         foreign key (USER_ID) references USER (ID),
---     constraint ANSWER_HEAD_TOI_ID_fk
---         foreign key (TOI_ID) references TOI (ID)
--- );
---
--- create table ANSWER
--- (
--- 	ID int auto_increment comment '回答ID'
--- 		primary key,
---     ANSWER_HEAD_ID int not null,
--- 	QUESTION_ID int not null comment '質問と紐づくID',
---     PICTURE_URL varchar(255) null comment '問題に画像を付ける場合',
--- 	ANSWER text null comment '回答内容',
---     INSERTED timestamp default current_timestamp,
---     MODIFIED timestamp default current_timestamp not null,
---     constraint ANSWER_HEAD_QUESTION_ID_fk
---         foreign key (ANSWER_HEAD_ID) references ANSWER_HEAD (ID),
---     constraint ANSWER_QUESTION_ID_fk
--- 		foreign key (QUESTION_ID) references QUESTION (ID)
--- );
---
--- create table TALK
--- (
--- 	ID int auto_increment comment '本ID'
--- 		primary key,
--- 	TOI_ID int not null comment '問題集と紐づくID',
--- 	USER_ID varchar(128) not null comment '所有者',
--- 	TALK_ID int null comment '返信した場合、紐付けをする',
--- 	COMMENT varchar(255) null comment 'コメント',
---     INSERTED timestamp default current_timestamp,
---     MODIFIED timestamp default current_timestamp not null,
--- 	constraint TALK_TALK_ID_fk
--- 		foreign key (TALK_ID) references TALK (ID),
--- 	constraint TALK_TOI_ID_fk
--- 		foreign key (TOI_ID) references TOI (ID)
--- );
+create table answer
+(
+    id uuid not null default uuid_in(md5(random()::text || clock_timestamp()::text)::cstring),
+    orderId int not null,
+    answer_head_id uuid not null,
+	question_id uuid not null,
+    picture_url varchar(255),
+    answer varchar(512) null,
+    inserted timestamp default current_timestamp not null,
+    modified timestamp default current_timestamp
+);
 
--- create table LIKES
--- (
---     ID int auto_increment comment 'いいね' primary key,
---     TOI_ID int null comment '問題集と紐づくID',
---     QUESTION_ID int null comment '質問と紐づくID',
---     ANSWER_HEAD_ID int null comment '回答まとめと紐づくID',
---     ANSWER_ID int null comment '回答まとめと紐づくID',
---     TALK_ID int null comment 'トークと紐づくID',
---     INSERTED timestamp default current_timestamp
--- );
+create table answer_head
+(
+    id uuid not null default uuid_in(md5(random()::text || clock_timestamp()::text)::cstring),
+    user_id uuid not null,
+    publish_flg varchar(1) not null default '1',
+    inserted timestamp default current_timestamp not null,
+    modified timestamp default current_timestamp
+);
 
-
--- create table user
--- (
--- 	ID varchar(128) not null comment 'USER ID'
--- 		primary key,
--- 	ID_TOKEN text not null comment '一時的アクセスのためのトークン',
--- 	ACCESS_TOKEN text not null comment '一時的アクセスのためのトークン',
--- 	REFRESH_TOKEN text not null comment 'アクセストークン更新のためのトークン',
--- 	INSERTED timestamp default current_timestamp,
---     MODIFIED timestamp default current_timestamp not null
--- );
+create table likes
+(
+    id uuid not null default uuid_in(md5(random()::text || clock_timestamp()::text)::cstring),
+    type varchar(5) not null,
+    foreign_id uuid not null,
+    inserted timestamp default current_timestamp not null
+);
