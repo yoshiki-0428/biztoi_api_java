79f75ca553346e81ebfbd00adffd56f749e38b0d
Yoshiki Ohashi <rrrtcdish2@gmail.com>
2020-03-12 09:12:12 +0900

    [fix] Heroku 環境 API開放Ver

--- src/main/java/com/biztoi/web/config/SecurityConfig.java
+++ src/main/java/com/biztoi/web/config/SecurityConfig.java
@@ -2,12 +2,14 @@ package com.biztoi.web.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
+    @Profile("!heroku")
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         // Disable default security.
@@ -16,17 +18,33 @@ public class SecurityConfig {
         http.csrf().disable();
         http.logout().disable();
 
+        // OAuth2
         http.oauth2Login();
-
-        // Add custom security.
-//        http.authenticationManager(this.authenticationManager);
-//        http.securityContextRepository(this.securityContextRepository);
+        http.logout();
 
         // authentication
-        // TODO del
-        http.authorizeExchange().pathMatchers("/**/**").permitAll();
-        http.authorizeExchange().pathMatchers("/api/auth").permitAll();
+        http.authorizeExchange().pathMatchers("/oauth2/**").permitAll();
+        http.authorizeExchange().pathMatchers("/api/books/").permitAll();
         http.authorizeExchange().anyExchange().authenticated();
         return http.build();
     }
+
+    @Profile("heroku")
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChainHeroku(ServerHttpSecurity http) {
+        // Disable default security.
+        http.httpBasic().disable();
+        http.formLogin().disable();
+        http.csrf().disable();
+
+        // OAuth2
+        http.oauth2Login();
+        http.logout();
+
+        // authentication
+        http.authorizeExchange().pathMatchers("/**/**").permitAll();
+        http.authorizeExchange().anyExchange().authenticated();
+        return http.build();
+    }
+
 }
