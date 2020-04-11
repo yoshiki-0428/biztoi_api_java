3e8d8af539cc98c968b80335584e51288f0fc30f
Yoshiki Ohashi <rrrtcdish2@gmail.com>
2020-04-11 20:54:15 +0900

    [fix] RefreshToken の更新部分の作成

---
+++ src/main/java/com/biztoi/web/filter/RefreshTokenFilter.java
@@ -0,0 +1,73 @@
+package com.biztoi.web.filter;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.*;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.DefaultReactiveOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.server.ServerOAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+
+// @see https://github.com/spring-cloud/spring-cloud-security/issues/175
+
+@Component
+public class RefreshTokenFilter implements WebFilter {
+
+    private final ReactiveOAuth2AuthorizedClientManager authorizedClientManager;
+
+    private static final Duration accessTokenExpiresSkew = Duration.ofMinutes(1);
+
+    public RefreshTokenFilter(ServerOAuth2AuthorizedClientRepository authorizedClientRepository,
+                              ReactiveClientRegistrationRepository clientRegistrationRepository) {
+        this.authorizedClientManager = createDefaultAuthorizedClientManager(clientRegistrationRepository, authorizedClientRepository);
+    }
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+        var refresh = exchange.getPrincipal()
+                .filter(principal -> principal instanceof OAuth2AuthenticationToken)
+                .cast(OAuth2AuthenticationToken.class)
+                .flatMap(this::authorizeClient)
+                .map(OAuth2AuthorizedClient::getAccessToken)
+                .map(token -> withBearerAuth(exchange, token))
+                .defaultIfEmpty(exchange).flatMap(chain::filter);
+        return chain.filter(exchange).and(refresh);
+    }
+
+    private static ReactiveOAuth2AuthorizedClientManager createDefaultAuthorizedClientManager(
+            ReactiveClientRegistrationRepository clientRegistrationRepository,
+            ServerOAuth2AuthorizedClientRepository authorizedClientRepository) {
+
+        final ReactiveOAuth2AuthorizedClientProvider authorizedClientProvider =
+                ReactiveOAuth2AuthorizedClientProviderBuilder.builder()
+                        .refreshToken(configurer -> configurer.clockSkew(accessTokenExpiresSkew))
+                        .clientCredentials(configurer -> configurer.clockSkew(accessTokenExpiresSkew))
+                        .build();
+        final DefaultReactiveOAuth2AuthorizedClientManager authorizedClientManager = new DefaultReactiveOAuth2AuthorizedClientManager(
+                clientRegistrationRepository, authorizedClientRepository);
+        authorizedClientManager.setAuthorizedClientProvider(authorizedClientProvider);
+
+        return authorizedClientManager;
+    }
+
+    private ServerWebExchange withBearerAuth(ServerWebExchange exchange, OAuth2AccessToken accessToken) {
+        return exchange.mutate().request(r -> r.headers(headers -> headers.setBearerAuth(accessToken.getTokenValue()))).build();
+    }
+
+    private Mono<OAuth2AuthorizedClient> authorizeClient(OAuth2AuthenticationToken oAuth2AuthenticationToken) {
+        final String clientRegistrationId = oAuth2AuthenticationToken.getAuthorizedClientRegistrationId();
+        return Mono.defer(() -> authorizedClientManager.authorize(createOAuth2AuthorizeRequest(clientRegistrationId, oAuth2AuthenticationToken)));
+    }
+
+    private OAuth2AuthorizeRequest createOAuth2AuthorizeRequest(String clientRegistrationId, Authentication principal) {
+        return OAuth2AuthorizeRequest.withClientRegistrationId(clientRegistrationId).principal(principal).build();
+    }
+
+}
