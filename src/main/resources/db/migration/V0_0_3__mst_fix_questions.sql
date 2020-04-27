8a942b126bbf1699cb30f402b011c80300930260
Yoshiki Ohashi <rrrtcdish2@gmail.com>
2020-04-27 12:08:42 +0900

    [add] new Questions https://trello.com/c/neH3qWhi/48-%E3%82%A2%E3%83%97%E3%83%AA%E3%81%AE%E5%95%8F%E9%A1%8C%E3%82%92%E3%82%82%E3%81%86%E4%B8%80%E5%BA%A6%E8%AA%AD%E3%81%BF%E3%82%84%E3%81%99%E3%81%84%E3%82%88%E3%81%86%E3%81%AB%E5%86%8D%E8%80%83%E3%81%99%E3%82%8B

---
+++ src/main/resources/db/migration/V0_0_3__mst_fix_questions.sql
@@ -0,0 +1,27 @@
+INSERT INTO public.mst_question (id, pattern_id, order_id, title, detail, example, step, answer_type, required) VALUES
+('1-1', 1, 1,
+ 'なぜこの本を読もうと思ったのか？',
+ 'この本は「あなたにとって」どのように役立ちそうなのか書いてみよう！',
+ '習慣化のコツを学び、筋トレを継続したい。夏までに腹筋を鍛えたい。',
+ '1', '0', '1');
+
+INSERT INTO public.mst_question (id, pattern_id, order_id, title, detail, example, step, answer_type, required) VALUES
+('1-2', 1, 2,
+ 'この本から学んだ知識は何か？',
+ '「すぐに使えるもの」や「重要だと思うもの」を書いていこう！',
+ '「If then プランニング」というテクニックがある。具体的には…',
+ '2', '0', '1');
+
+INSERT INTO public.mst_question (id, pattern_id, order_id, title, detail, example, step, answer_type, required) VALUES
+('1-3', 1, 3,
+ 'この本を一言で要約するならどう書くか？',
+ '「この本の帯を自分が書くなら…」と考えてみよう！',
+ '怠ける自分を操るための最強の心理学',
+ '2', '0', '0');
+
+INSERT INTO public.mst_question (id, pattern_id, order_id, title, detail, example, step, answer_type, required) VALUES
+('1-4', 1, 4,
+ 'この本から得た知識を使って行動プランを考えよう！',
+ '明日までに実践できるプランにしよう！',
+ 'もしスマホを触りそうになったら、腹筋を５回する。',
+ '3', '0', '1');
